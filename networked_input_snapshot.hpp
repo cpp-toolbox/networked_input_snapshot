2cdf5b84890478262b24dd7f589f924ad285040c
cuppajoeman <ccn@cuppajoeman.com>
2024-10-31 02:28:19 -0400

    feat: add in weapon selection actions

--- networked_input_snapshot.hpp
+++ networked_input_snapshot.hpp
@@ -10,12 +10,21 @@ class NetworkedInputSnapshot {
     // these are guarenteed to be not -1 when received on the server.
     uint64_t client_input_history_insertion_time_epoch_ms = -1;
     double time_delta_used_for_client_side_processing_ms = -1;
+
     bool left_pressed = false;
     bool right_pressed = false;
     bool forward_pressed = false;
     bool backward_pressed = false;
     bool jump_pressed = false;
+    // one time style events:
     bool fire_pressed = false;
+    bool select_rocket_launcher_pressed = false;
+    bool select_sniper_rifle_pressed = false;
+    bool select_lightning_gun_pressed = false;
+    bool select_shot_gun_pressed = false;
+    bool select_grenade_launcher_pressed = false;
+    bool select_knife_pressed = false;
+
     double mouse_position_x = 0.0, mouse_position_y = 0.0;
 
     NetworkedInputSnapshot() {};
